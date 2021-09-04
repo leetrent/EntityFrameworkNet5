6f8ec5780964444d70c66482774d499bd6c1a982
LeeTrent <lee.trent.1@gmail.com>
2021-09-04 11:22:40 -0500

    Adds insert commands

--- EntityFrameworkNet5.ConsoleApp/Program.cs
+++ EntityFrameworkNet5.ConsoleApp/Program.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using EntityFrameworkNet5.Data;
+using EntityFrameworkNet5.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EntityFrameworkNet5.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly FootballLeageDbContext context = new FootballLeageDbContext();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ////////////////////////////////////////////////////////////
+            // CREATE LEAGUE
+            ////////////////////////////////////////////////////////////
+            //League league1 = new() { Name = "Red Stripe Premier League" };
+            //await context.Leagues.AddAsync(league1);
+            //await context.SaveChangesAsync();
+
+            ////////////////////////////////////////////////////////////
+            // ADD NEWLY CREATED TEAMS TO LEAGUE
+            ////////////////////////////////////////////////////////////
+            //await AddTeamsWithLeague(league1);
+            //await context.SaveChangesAsync();
+
+            ////////////////////////////////////////////////////////////
+            // CREATE NEW LEAGUE AND NEW TEAM AT THE SAME TIME
+            ////////////////////////////////////////////////////////////
+            //League league2 = new() { Name = "Bundesliga" };
+            //Team team2 = new() {Name = "Bayern Munich", League = league2 };
+            //await context.AddAsync(team2); // Entity type not specified. EF Core infers entity type.
+            //await context.SaveChangesAsync(); 
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
         }
+
+        static async Task AddTeamsWithLeague(League league)
+        {
+            var teams = new List<Team>
+            {
+                new Team
+                {
+                    Name = "Juventus",
+                    LeagueId = league.Id // Foreign Key Id
+                },
+                new Team
+                {
+                    Name = "AC Milan",
+                    LeagueId = league.Id // Foreign Key Id
+                },
+                new Team
+                {
+                    Name = "AS Roma",
+                    League = league // Foreign Key Navigation Object
+                }
+            };
+
+            //// Operation to add multiple objects to database in one call.
+            await context.AddRangeAsync(teams);
+        }
+
     }
 }
